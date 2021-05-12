7f364ed9523e13ae08b16c7e75bf1c3412965d2d
Nick DeJesus <nick@nicks-air.myfiosgateway.com>
2021-05-12 16:58:02 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-const withTM = require('next-transpile-modules')(['next-slicezone', 'essential-slices']);
-
-module.exports = withTM();
+const { withPlugins } = require('next-compose-plugins');
+const nextConfig = { target: 'serverless' };
+const withTM = require('next-transpile-modules')(['next-slicezone']);
+module.exports = withPlugins([withTM()], nextConfig);
