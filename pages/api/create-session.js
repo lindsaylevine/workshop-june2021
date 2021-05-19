ebe28b622fc75155b48b17169d0d348d549e4a27
Nick DeJesus <nick@nicks-air.myfiosgateway.com>
2021-05-19 12:14:44 -0400

    setting up checkout function

--- pages/api/create-session.js
+++ pages/api/create-session.js
@@ -1,38 +1,46 @@
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 
-const getCheckoutData =
-  require('use-shopping-cart/utilities/old-utils').getCheckoutData;
-
 // const validateCartItems =
 //   require('use-shopping-cart/utilities/serverless').validateCartItems;
 
 // const inventory = require('../data/products.json');
 
 exports.handler = async (event) => {
-  try {
-    const productJSON = JSON.parse(event.body);
+  const products = await queryRepeatableDocuments(
+    (docs) => docs.type === 'products'
+  );
 
-    // const line_items = validateCartItems(inventory, productJSON);
+  console.log(products);
 
-    const line_items = getCheckoutData(productJSON);
+  return {
+    statusCode: 200,
+    body: 'lol',
+  };
 
-    const session = await stripe.checkout.sessions.create({
-      mode: 'payment',
-      payment_method_types: ['card'],
-      billing_address_collection: 'auto',
-      shipping_address_collection: {
-        allowed_countries: ['US', 'CA'],
-      },
-      success_url: `${process.env.URL}/success.html`,
-      cancel_url: process.env.URL,
-      line_items,
-    });
+  // try {
+  //   const productJSON = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ sessionId: session.id }),
-    };
-  } catch (error) {
-    console.error(error);
-  }
+  //   // const line_items = validateCartItems(inventory, productJSON);
+
+  //   const line_items = getCheckoutData(productJSON);
+
+  //   const session = await stripe.checkout.sessions.create({
+  //     mode: 'payment',
+  //     payment_method_types: ['card'],
+  //     billing_address_collection: 'auto',
+  //     shipping_address_collection: {
+  //       allowed_countries: ['US', 'CA'],
+  //     },
+  //     success_url: `${process.env.URL}/success.html`,
+  //     cancel_url: process.env.URL,
+  //     line_items,
+  //   });
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ sessionId: session.id }),
+  //   };
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
